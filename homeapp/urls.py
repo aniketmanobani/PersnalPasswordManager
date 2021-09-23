091ba1511f25fd8b5110ad22a50b58084c38535f
Aniket <aniketmanobani@gmail.com>
2021-09-23 21:10:15 +0530

    Profle section  added

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -10,4 +10,14 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('create', views.createPassword, name='create_password'),
     path('viewdetails/<int:id>', views.viewDetails, name='viewdetails'),
+    path('apps-passwords/', views.appsPassword, name='apps_password'),
+    path('website-passwords/', views.websitePassword, name='website_password'),
+
+    path('other-passwords/', views.otherPassword, name='other_password'),
+    path('delete/<int:id>', views.deletePass, name='deletePass'),
+    path('editpass/<int:id>', views.editpass, name='editpass'),
+    path('search/', views.search, name='search'),
+    path('profile/', views.profile, name='profile'),
+    path('settings/', views.settings, name='settings'),
+
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -2,16 +2,30 @@ import datetime
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import request
+from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 from homeapp.models import PasswordStore
 
 
 @login_required(login_url='login')
 def index(request):
-    return render(request, 'index.html')
+    ps = PasswordStore.objects.filter(user_id=request.user).order_by('-id')
+    page = request.GET.get('page', 1)
+    no_of_item = 10
+    paginator = Paginator(ps, no_of_item)
+    try:
+        list = paginator.page(page)
+    except PageNotAnInteger:
+        list = paginator.page(1)
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)
+
+    return render(request, 'index.html', {'list': list})
 
 
 def login(request):
@@ -98,7 +112,8 @@ def register(request):
                 auth.login(request, user)
                 return redirect('index')
             else:
-                messages.warning(request, "Authentication error, please try again")
+                messages.warning(
+                    request, "Authentication error, please try again")
                 render(request, 'register.html')
 
     return render(request, 'register.html')
@@ -147,7 +162,8 @@ def createPassword(request):
             messages.warning(request, "Password Saved Succesfully")
             # return render(request, 'create_password.html')
 
-    recent = PasswordStore.objects.filter(user_id=request.user).order_by('-id')[:10]
+    recent = PasswordStore.objects.filter(
+        user_id=request.user).order_by('-id')[:10]
     return render(request, 'create_password.html', {'recent': recent, 'range': range(10)})
 
 
@@ -158,12 +174,153 @@ def viewDetails(request, id):
 
     if request.method == "POST":
         if 'password' in request.POST.keys() and len(request.POST.get('password')) > 0:
-            verifyPass = auth.authenticate(username=request.user.username, password=request.POST.get('password'))
+            verifyPass = auth.authenticate(
+                username=request.user.username, password=request.POST.get('password'))
             if verifyPass is not None:
                 showPassword = True
+            else:
+                messages.warning(request, "Incorrect Password")
     if p.exists():
         print(p)
         return render(request, 'viewdetails.html', {'info': p[0], 'showPassword': showPassword})
     else:
         return render(request, 'not_found.html')
     return render(request, 'viewdetails.html')
+
+
+@login_required(login_url='login')
+def appsPassword(request):
+    ps = PasswordStore.objects.filter(
+        user_id=request.user, app_type='apps').order_by('-id')
+    page = request.GET.get('page', 1)
+    no_of_item = 10
+    paginator = Paginator(ps, no_of_item)
+    try:
+        list = paginator.page(page)
+    except PageNotAnInteger:
+        list = paginator.page(1)
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)
+    return render(request, 'apps_password.html', {'list': list})
+
+
+@login_required(login_url='login')
+def websitePassword(request):
+    ps = PasswordStore.objects.filter(
+        user_id=request.user, app_type='website').order_by('-id')
+    page = request.GET.get('page', 1)
+    no_of_item = 10
+    paginator = Paginator(ps, no_of_item)
+    try:
+        list = paginator.page(page)
+    except PageNotAnInteger:
+        list = paginator.page(1)
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)
+    return render(request, 'website_passwords.html', {'list': list})
+
+
+@login_required(login_url='login')
+def otherPassword(request):
+    ps = PasswordStore.objects.filter(
+        user_id=request.user, app_type='others').order_by('-id')
+    page = request.GET.get('page', 1)
+    no_of_item = 10
+    paginator = Paginator(ps, no_of_item)
+    try:
+        list = paginator.page(page)
+    except PageNotAnInteger:
+        list = paginator.page(1)
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)
+    return render(request, 'others_password.html', {'list': list})
+
+
+@csrf_exempt
+@login_required(login_url='login')
+def deletePass(request, id):
+    if request.method == "POST":
+        if 'password' in request.POST.keys():
+            print(request.POST.get('password'))
+            au = auth.authenticate(
+                username=request.user.username, password=request.POST.get('password'))
+            if au is not None:
+                p = PasswordStore.objects.filter(user_id=request.user, id=id)
+                if p.exists():
+                    p.delete()
+                    print("del")
+                    return HttpResponse("Deleted")
+                else:
+                    print('not del')
+                    return HttpResponse("Password Does Not Match")
+            else:
+                return HttpResponse("Password Does Not Matched")
+
+    return HttpResponse("Post method is required")
+
+
+@login_required(login_url='login')
+def editpass(request, id):
+    p = PasswordStore.objects.filter(user_id=request.user, id=id)
+    if p.exists():
+        if request.method == "POST":
+            formData = request.POST
+            errors = {}
+            email = ""
+            username = ""
+            otherDetails = ''
+            if 'apptype' not in formData.keys() or len(formData.get('apptype')) < 2:
+                errors['apptype'] = "App Type is required"
+            if 'createpassword' not in formData.keys() or formData.get('createpassword') == "":
+                errors['password'] = 'Password field is required'
+            if 'email' in formData.keys():
+                email = formData.get('email')
+            if 'username' in formData.keys():
+                username = formData.get('username')
+            if 'otherDetails' in formData.keys():
+                otherDetails = formData.get('otherDetails')
+
+            if 'app_web_name' not in formData.keys() or len(formData.get('app_web_name')) < 2:
+                errors['app_web_name'] = "App/Website name is required"
+
+            if errors:
+                for e in errors.values():
+                    messages.warning(request, e)
+            else:
+                pt = PasswordStore.objects.filter(user_id=request.user, id=id)
+                app_or_web = formData.get('app_web_name')
+                apptype = formData.get('apptype')
+                password = formData.get('createpassword')
+
+                pt = pt[0]
+                pt.apptype = apptype
+                pt.app_or_web = app_or_web
+                pt.email = email
+                pt.username = username
+                pt.app_pass = password
+                pt.other_detail = otherDetails
+                pt.updated_on = datetime.date.today()
+                pt.save()
+                messages.warning(request, "Changes Saved Succesfully")
+        return render(request, 'editapps.html', {'info': p[0]})
+    else:
+        return render(request, 'not_found.html')
+
+
+@login_required(login_url='login')
+def search(request):
+    if "search_text" in request.GET.keys() and len(request.GET.get('search_text')) > 0:
+        search_text = request.GET.get('search_text')
+        res = PasswordStore.objects.filter(app_or_web__contains=search_text).order_by('-id')[:50]
+        return render(request, "search.html",{'search_text':search_text,'res':res})
+
+    return render(request, "search.html")
+
+
+@login_required(login_url='login')
+def profile(request):
+    return render(request,'profile.html')
+
+@login_required(login_url='login')
+def settings(request):
+    return render(request,'settings.html')
